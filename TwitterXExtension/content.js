7f18edd11e1871de0aac43f181dd7299c7ffdd13
badjtsx <90721515+badjtsx@users.noreply.github.com>
2024-07-15 16:13:08 +0300

    Create content.js

---
+++ TwitterXExtension/content.js
@@ -0,0 +1,10 @@
+const fontCss = `
+    .css-175oi2r button {
+        display: none !important;
+    }
+`;
+    
+
+const styleElement = document.createElement('style');
+styleElement.textContent = fontCss;
+document.head.appendChild(styleElement);       
